20c8ce299a862240cededece67ba3729c8137598
Rohan Singh <haitomnsrohan2024@gmail.com>
2024-10-27 22:51:50 +0545

    database updated

--- database/studentmsdb.sql
+++ database/studentmsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 27, 2024 at 06:30 AM
+-- Generation Time: Oct 27, 2024 at 05:04 PM
 -- Server version: 8.3.0
 -- PHP Version: 8.2.18
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `studentmsdb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addfeesgroup`
+--
+
+DROP TABLE IF EXISTS `addfeesgroup`;
+CREATE TABLE IF NOT EXISTS `addfeesgroup` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `feesGroupID` varchar(30) NOT NULL,
+  `feesGroup` varchar(30) NOT NULL,
+  `feesGroupAmount` varchar(15) NOT NULL,
+  `remark` varchar(50) NOT NULL,
+  `feesGroupStatus` varchar(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `addfeesgroup`
+--
+
+INSERT INTO `addfeesgroup` (`ID`, `feesGroupID`, `feesGroup`, `feesGroupAmount`, `remark`, `feesGroupStatus`) VALUES
+(5, 'GID1', 'Tuition Fee', 'Rs  1500', 'The money you pay to be taught', 'Active'),
+(6, 'FGID2', 'Bus fees', 'Rs  600', 'The money you pay is not taught.', 'Inactive');
 
 -- --------------------------------------------------------
 
@@ -45,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `addstudent` (
   `bloodGroup` varchar(3) COLLATE utf8mb4_general_ci NOT NULL,
   `house` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `religion` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `category` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `feesGroup` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `caste` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
   `primaryContact` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
   `emailOfstudent` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
@@ -88,15 +113,16 @@ CREATE TABLE IF NOT EXISTS `addstudent` (
   `ifscNumber` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
   `otherInfo` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `addstudent`
 --
 
-INSERT INTO `addstudent` (`ID`, `imageOfStudent`, `academicYear`, `admissionNumber`, `admissionDate`, `rollNumber`, `studentStatus`, `fnameOfStudent`, `lnameOfStudent`, `class`, `section`, `gender`, `dateOfBirth`, `bloodGroup`, `house`, `religion`, `category`, `caste`, `primaryContact`, `emailOfstudent`, `motherTongue`, `languageKnown`, `imageOfFather`, `fatherName`, `emailOfFather`, `fatherContact`, `fatherProfession`, `imageOfMother`, `motherName`, `emailOfMother`, `motherContact`, `motherProfession`, `guardianName`, `guardianRelation`, `guardianContact`, `guardianEmail`, `guardianOccupation`, `guardianAddress`, `imageOfGuardian`, `currentAddressOfStudent`, `permanentAddressOfStudent`, `districtOfStudent`, `provinceOfStudent`, `transportRoute`, `vehicleNumber`, `pickUpPoint`, `hostel`, `hostelRoomNumber`, `documentOfBirthCertificate`, `documentOfTransferCertificate`, `allergiesOfStudent`, `medicationOfStudent`, `previousSchoolName`, `previousSchoolAddress`, `bankName`, `branchOfBank`, `ifscNumber`, `otherInfo`) VALUES
+INSERT INTO `addstudent` (`ID`, `imageOfStudent`, `academicYear`, `admissionNumber`, `admissionDate`, `rollNumber`, `studentStatus`, `fnameOfStudent`, `lnameOfStudent`, `class`, `section`, `gender`, `dateOfBirth`, `bloodGroup`, `house`, `religion`, `feesGroup`, `caste`, `primaryContact`, `emailOfstudent`, `motherTongue`, `languageKnown`, `imageOfFather`, `fatherName`, `emailOfFather`, `fatherContact`, `fatherProfession`, `imageOfMother`, `motherName`, `emailOfMother`, `motherContact`, `motherProfession`, `guardianName`, `guardianRelation`, `guardianContact`, `guardianEmail`, `guardianOccupation`, `guardianAddress`, `imageOfGuardian`, `currentAddressOfStudent`, `permanentAddressOfStudent`, `districtOfStudent`, `provinceOfStudent`, `transportRoute`, `vehicleNumber`, `pickUpPoint`, `hostel`, `hostelRoomNumber`, `documentOfBirthCertificate`, `documentOfTransferCertificate`, `allergiesOfStudent`, `medicationOfStudent`, `previousSchoolName`, `previousSchoolAddress`, `bankName`, `branchOfBank`, `ifscNumber`, `otherInfo`) VALUES
 (3, 'studentDocuments/profilePictures/671a0b1e4fe48_Screenshot 2024-10-24 124154.png', 'June 2023/24', 'AD001', '2024-10-09', 'RL001', 'Active', 'Haitomns', 'Rohit', 'III', 'A', 'Male', '2024-10-03', 'O -', 'Blue', 'Muslim', 'BC', 'Kurmi', '9820998994', 'sankar8242@gmail.com', 'English', 'English, Spanish', 'studentDocuments/profilePictures/671a0b1e7932f_Screenshot 2024-10-24 142100.png', 'Harishankar kurmi', 'harishankarpatel144@gmail.com', '9852635652', 'Worker', 'studentDocuments/profilePictures/671a0b1ea5bb7_Screenshot 2024-10-23 230518.png', 'Laxmi Devi', 'laxmidevi22@gmail.com', '9822550000', 'HouseWife', 'Gaurishankar', 'Uncle', '9812110620', 'gauri122@gmail.com', 'Operator', 'Pipara-14', 'studentDocuments/profilePictures/671a0b1ecf656_Screenshot 2024-10-23 145438.png', 'Bhairahwa-14, Biratnagar', 'Parwanipur-21, Birgunj,parsa', 'Parsa', 'Madhesh Pradesh', 'NewYork', 'AM 54548', 'Cincinatti', 'Phoenix Residence', '22', 'studentDocuments/profilePictures/6719fb91afc74_haitomns.jpg', 'studentDocuments/profilePictures/6719fb91b04ae_rohan.jpg', 'Allergy, Skin Allergy', 'Medecine Name', 'Shree Nrisingh Madhyamik Vidhyalay', 'Piparamathh-14, Birgunj', 'Nepal Central Bank', 'Kathmandu', '12340008907654', 'sdzfxghgjhkjglkhl'),
-(4, 'studentDocuments/profilePictures/671bb2679bc42_singh.jpg', 'June 2023/24', 'AD002', '2024-10-02', 'RL001', 'Active', 'Rohan', 'Sharma', 'III', 'B', 'Male', '2024-11-01', 'A +', 'Red', 'Sikh', 'ST', 'Kurmi', '9820998994', 'haitomnsrohan2024@gmail.com', 'English', 'English, Spanish', 'studentDocuments/profilePictures/671a14e563941_Screenshot 2024-09-06 163741.png', 'Harishankar kurmi', 'harishankarpatel144@gmail.com', '9852635652', 'Worker', 'studentDocuments/profilePictures/671a14e563bcd_Screenshot 2024-09-18 125846.png', 'Laxmi Devi', 'laxmidevi22@gmail.com', '9845190056', 'HouseWife', 'Gaurishankar', 'Uncle', '9812110620', 'gauri122@gmail.com', 'Operator', 'Pipara-14', 'studentDocuments/profilePictures/671a14e563df8_Screenshot 2024-09-13 203337.png', 'Bhairahwa-14, Biratnagar', 'Parwanipur-21, Birgunj,parsa', 'Parsa', 'Bagmati Province', 'NewYork', 'AM 54548', 'Illinois', 'Tranquil Haven', '20', 'studentDocuments/profilePictures/671a15650b031_Screenshot 2024-10-24 145309.png', 'studentDocuments/profilePictures/671a15650b26d_Screenshot (3).png', 'Allergy, Skin Allergy', 'Medecine Name', 'ghp', 'Piparamathh-14, Birgunj', 'Nepal Central Bank', 'murli', '12340008907654', 'jgfghfxddfgfgl;');
+(4, 'studentDocuments/profilePictures/671bb2679bc42_singh.jpg', 'June 2023/24', 'AD002', '2024-10-02', 'RL001', 'Active', 'Rohan', 'Sharma', 'III', 'B', 'Male', '2024-11-01', 'A +', 'Red', 'Sikh', 'ST', 'Kurmi', '9820998994', 'haitomnsrohan2024@gmail.com', 'English', 'English, Spanish', 'studentDocuments/profilePictures/671a14e563941_Screenshot 2024-09-06 163741.png', 'Harishankar kurmi', 'harishankarpatel144@gmail.com', '9852635652', 'Worker', 'studentDocuments/profilePictures/671a14e563bcd_Screenshot 2024-09-18 125846.png', 'Laxmi Devi', 'laxmidevi22@gmail.com', '9845190056', 'HouseWife', 'Gaurishankar', 'Uncle', '9812110620', 'gauri122@gmail.com', 'Operator', 'Pipara-14', 'studentDocuments/profilePictures/671a14e563df8_Screenshot 2024-09-13 203337.png', 'Bhairahwa-14, Biratnagar', 'Parwanipur-21, Birgunj,parsa', 'Parsa', 'Bagmati Province', 'NewYork', 'AM 54548', 'Illinois', 'Tranquil Haven', '20', 'studentDocuments/profilePictures/671a15650b031_Screenshot 2024-10-24 145309.png', 'studentDocuments/profilePictures/671a15650b26d_Screenshot (3).png', 'Allergy, Skin Allergy', 'Medecine Name', 'ghp', 'Piparamathh-14, Birgunj', 'Nepal Central Bank', 'murli', '12340008907654', 'jgfghfxddfgfgl;'),
+(5, 'studentDocuments/profilePictures/671e6731eae8a_Screenshot 2024-05-28 021649.png', 'June 2023/24', 'AD003', '2024-10-15', 'RL004', 'Active', 'Haitomns', 'Rohan', 'II', 'A', 'Male', '2024-10-10', 'A+', 'Red', 'Hindu', 'GID1', 'Kurmi', '9820998994', 'sankar8242@gmail.com', 'English', 'English, Spanish', 'studentDocuments/profilePictures/671e63511e9e3_rohan.jpg', 'Harishankar kurmi', 'harishankarpatel144@gmail.com', '9852635652', 'Worker', 'studentDocuments/profilePictures/671e63511f5b4_haitomns.jpg', 'Laxmi Devi', 'laxmidevi22@gmail.com', '9822550000', 'HouseWife', 'Gaurishankar', 'Uncle', '9812110620', 'gauri122@gmail.com', 'Operator', 'Pipara-14', 'studentDocuments/profilePictures/671e63511f76e_Screenshot 2024-10-27 201740.png', 'Bhairahwa-14, Biratnagar', 'Parwanipur-21, Birgunj,parsa', 'Kathmandu', 'Bagmati Province', 'Denver', 'AM 64528', 'Cincinatti', 'Phoenix Residence', '20', 'studentDocuments/profilePictures/671e63511f9b8_Screenshot (2).png', 'studentDocuments/profilePictures/671e63511fe05_Screenshot (3).png', 'Allergy, Skin Allergy', 'Medecine Name', 'Shree Nrisingh Madhyamik Vidhyalay', 'Piparamathh-14, Birgunj', 'Nepal Central Bank', 'Kathmandu', '12340008907654', 'i m good');
 
 -- --------------------------------------------------------
 
@@ -213,14 +239,20 @@ CREATE TABLE IF NOT EXISTS `smslogindata` (
   `userType` int NOT NULL,
   `userLastLogin` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`usersLoginID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `smslogindata`
 --
 
 INSERT INTO `smslogindata` (`usersLoginID`, `userEmail`, `userUsername`, `userPassword`, `userType`, `userLastLogin`) VALUES
-(7, 'haitomnsrohan2024@gmail.com', 'Haitomnsrohan', '$2y$10$d1kNlD4byLrUtVAu53kSpe2RevTS0QGFlULPMqGOTa/KX2ycWqtiW', 3, '2024-10-25 23:45:20');
+(7, 'haitomnsrohan2024@gmail.com', 'Haitomnsrohan', '$2y$10$d1kNlD4byLrUtVAu53kSpe2RevTS0QGFlULPMqGOTa/KX2ycWqtiW', 3, '2024-10-25 23:45:20'),
+(8, 'haitomrohan2024@gmail.com', 'Haito', '$2y$10$wJWA5eY2wLKmBaPg0dTrI.tKE1eSsC/cw0W6F1AjrwnUc2tqc3iG2', 3, '2024-10-27 12:22:16'),
+(9, 'haitorohan2024@gmail.com', 'Hait', '$2y$10$GU3ecntnOGlmf4hBs6UBjOOzTYKx3Q6jQ/bYEgXdoQ4Xp0daw7bZG', 3, '2024-10-27 12:22:36'),
+(10, 'sankar@gmail.com', 'Rohan', '$2y$10$./kSAyhwAbfDsp0PmxvJPeqHqVBngnop/rrc/IpSWtQTSZWQhpZ5C', 3, '2024-10-27 12:33:32'),
+(11, 'ram@gmail.com', 'Rambabu', '$2y$10$ZJsjcHt10b3YAHxXdP9/Neq.ZS17/1pSM8VGEV2Pi1LpNjnzkqs8q', 3, '2024-10-27 12:46:10'),
+(12, 'sankar12@gmail.com', 'Haitomns', '$2y$10$9raxru8cS/XV9a/pK2Rh9unu1SaRdPw6ofnwiQC7dTYSCBTcLq6ti', 3, '2024-10-27 19:34:45'),
+(13, 'sankar123@gmail.com', 'Haitomnsroha', '$2y$10$erKgCBO1AmX2QMTV8iP.7.M4fXxU8/gKUxzIhaI9GEKqAdGTdihi2', 3, '2024-10-27 22:48:00');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
